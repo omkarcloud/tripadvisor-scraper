5c068e1c19a5daf5d634c2a9bd72d9c5cad8d26b
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2025-11-17 20:02:41 +0530

    fixes

--- src/tripadvisor/__init__.py
+++
@@ -1,1 +0,0 @@
-from .tripadvisor_scraper import Tripadvisor
--- src/tripadvisor/utils.py
+++
@@ -1,6 +0,0 @@
-default_request_options = {
-    "raise_exception": False,
-    "create_error_logs": False,
-    "close_on_crash": True,  # When Ready for Production change to True
-    "output": None,  # When Ready for Production Uncomment
-}
